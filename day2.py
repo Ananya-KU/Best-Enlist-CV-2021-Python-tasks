a128bb7dcdf0b70a00797443bac370f7355a19d1
Ananya K U <74902847+Ananya-KU@users.noreply.github.com>
2021-06-13 15:24:00 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,66 @@
+# How to print a value?
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+# Assigning String to Variable
+Hours = "thirty"
+print(Hours)
+
+# Indexing using String
+Days = "Thirty days"
+print(Days[3])
+
+# How to print the particular character from certain text?
+Challenge = "I will win"
+print(Challenge[7:10])
+
+# Print the length of Character
+Challenge = "I will win"
+print(len(Challenge))
+
+# Convert String into lower character
+Challenge = "I will win"
+print(Challenge.lower())
+
+# String Concatenation–Joining two strings
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+# Adding space during concatenation
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+# case fold() - Usage
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+text = "Thirty days and Thirty hours"
+x = text.capitalize()
+print(x)
+
+text = "Thirty days and Thirty hours"
+x = text.find("a")
+print(x)
+
+text = "Thirty days and Thirty hours"
+x = text.isalpha()
+print(x)
+
+y = "hello python"
+z = y.isalpha()
+print(z)
+
+text = "Thirty days and Thirty hours"
+x = text.isalnum()
+print(x)
+
+txt = "Text30"
+x = txt.isalnum()
+print(x)
+
+
