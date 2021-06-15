767f1c801a7eccce152e72e7dce75b63042e7d77
Ananya K U <74902847+Ananya-KU@users.noreply.github.com>
2021-06-15 11:31:11 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,28 @@
+# Create three variables (a,b,c) to same value of any integer & do the following
+# Divide a by 10
+# Multiply b by 50
+# Add c value by 60
+a = b = c = 30
+a = a / 10
+b = b * 50
+c = c + 60
+print(a)
+print(b)
+print(c)
+
+# Create a String variable of 5 characters and replace the 3rd character with G
+string = 'About'
+print(string.replace('o', 'G'))
+
+# Create two values (a,b) of int,float data type & convert the vise versa, Hint : convert a from int to float
+# datatype & b from float to int datatype
+x = 10  # int
+y = 6.8  # float
+# convert from int to float:
+a = float(x)
+# convert from float to int:
+b = int(y)
+print(a)
+print(b)
+print(type(a))
+print(type(b))
