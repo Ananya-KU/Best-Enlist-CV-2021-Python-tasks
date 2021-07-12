ff8d3b455407fe5de0f47a7a1ff5c374b0d0f211
Ananya K U <74902847+Ananya-KU@users.noreply.github.com>
2021-07-12 09:35:29 +0530

    Add files via upload

---
+++ Day26.py
@@ -0,0 +1,11 @@
+# Enumerate a python list and try to print the counter with the list value
+
+list1= ['ananya','pooja','harshitha','anu','nithin']
+print(list(enumerate(list1)))
+
+print(enumerate(list1))
+
+# Enumerate a python tuple and try to print the counter with the tuple value
+
+tuple1=('anusha','shravya','priya','karthik','satish')
+print(tuple(enumerate(tuple1)))
